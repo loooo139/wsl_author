b2f4b58d75577d7bf36c9ddea34d286320088d67
lixuefeng <loooo139@gmail.com>
2020-10-25 23:27:21 +0800

    hah

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@
 @Author: li xuefeng
 @Date: 2020-07-25 01:06:38
 
-LastEditTime: 2020-10-25 23:15:08
+LastEditTime: 2020-10-25 23:27:18
 LastEditors: lixf
 @Description: 
 FilePath: \wsl_author\crawler.py
@@ -67,7 +67,7 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 # options.add_experimental_option('mobileEmulation', mobileEmulation)
-# options.add_argument('--headless')
+options.add_argument('--headless')
 # 更换头部
 options.add_argument(
     'user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"'
